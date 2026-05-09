731a0dbb4c67ce2589f28bb682f9dfcf8bad564c
Xustavo <gugaxavier2005@gmail.com>
2026-05-09 11:58:49 -0300

    feat: implement distance algorithms in project

---
+++ src/distances.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def euclidean_distance(x, y):
+    """
+    Calcula a distância euclidiana entre dois vetores x e y.
+
+    params:
+        x: array, vetor de características
+        y: array, vetor de características
+    returns:
+        float, a distância euclidiana entre x e y
+    """
+    return np.sqrt(np.sum((x - y)** 2))
+
+def manhattan_distance(x, y):
+    """
+    Calcula a distância de Manhattan entre dois vetores x e y.
+
+    params:
+        x: array, vetor de características
+        y: array, vetor de características
+    returns:
+        float, a distância de Manhattan entre x e y
+    """
+    return np.sum(np.abs(x - y))
+
+def minkowski_distance(x, y, p):
+    """
+    Calcula a distância de Minkowski entre dois vetores x e y.
+
+    params:
+        x: array, vetor de características
+        y: array, vetor de características
+        p: int, ordem da distância de Minkowski
+    returns:
+        float, a distância de Minkowski entre x e y
+    """
+    return np.sum(np.abs(x - y) ** p) ** (1 / p)
